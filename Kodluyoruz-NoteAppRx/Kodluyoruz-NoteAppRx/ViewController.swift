d3374b91d0ccb05d32a2facdff84f543bd7efc9a
yildizkoray <koray.yildiz@gmail.com>
2019-09-13 23:17:24 -0700

    Project Start

---
+++ Kodluyoruz-NoteAppRx/Kodluyoruz-NoteAppRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kodluyoruz-NoteAppRx
+//
+//  Created by XCODE on 13.09.2019.
+//  Copyright © 2019 XCODE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
